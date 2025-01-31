6127b99b36e2b0790f2bd103dd84a1b3ec5d2338
YogeshPatel8910 <yogeshpatel8910@gmail.com>
2025-01-31 18:24:01 +0530

    added - roletable

---
+++ src/main/java/com/example/finalProject/model/Role.java
@@ -0,0 +1,19 @@
+package com.example.finalProject.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+}
+
